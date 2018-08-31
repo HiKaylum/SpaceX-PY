a7195060d5a0a96bec6890ea50b04de39d24d920
Kaylum Lally <kaylum@hikaylum.me>
2018-08-31 19:52:31 +0100

    setup.py for publishing

---
+++ setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="spacex_py",
+    version="0.0.1",
+    author="Kaylum Lally",
+    author_email="kaylum@hikaylum.me",
+    description="Python wrapper for the SpaceX API",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="hhttps://github.com/HiKaylum/SpaceX-PY",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=[
+        "requests"
+    ]
+)
---
+++ spacex_py/__init__.py
@@ -0,0 +1,1 @@
+name = "spacex_py"
